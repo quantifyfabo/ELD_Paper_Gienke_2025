f258958ce8fc293e1fe7ed1e8bf5fc38575624d4
quantifyfabo <134743163+quantifyfabo@users.noreply.github.com>
2026-09-07 12:27:50 +0300

    Change dataset file paths to relative paths
    Updated file paths to be relative instead of absolute.

--- 01_Load_Data.R
+++ 01_Load_Data.R
@@ -19,18 +19,18 @@ library(ggalluvial)
 
 # load datasets based on the I'll be back paper's CodeBook
 
-civ_confl_rep_01 <- read_dta('/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/LIE_Folder_Code_Data/Civil Conflict Replication Data.dta')
-irr_trans_rep_02 <- read_dta('/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/LIE_Folder_Code_Data/Irregular Transitions Replication Data.dta')
-coups_rep_03 <- read_dta('/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/LIE_Folder_Code_Data/Coups Replication Data.dta')
-protest_rep_04 <- read_dta('/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/LIE_Folder_Code_Data/Protests Replication Data.dta')
+civ_confl_rep_01 <- read_dta('/Civil Conflict Replication Data.dta')
+irr_trans_rep_02 <- read_dta('/Irregular Transitions Replication Data.dta')
+coups_rep_03 <- read_dta('/Coups Replication Data.dta')
+protest_rep_04 <- read_dta('/Protests Replication Data.dta')
 
 # created LIE Leader list from PDF
-lie_leaderlist_v2 <- read_excel('/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/lie_leader_list.xlsx')
-lae_raw <- read.csv('/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/LIE_LAE_Merging/LAE_DATA.csv')
+lie_leaderlist_v2 <- read_excel('/lie_leader_list.xlsx')
+lae_raw <- read.csv('/LIE_LAE_Merging/LAE_DATA.csv')
 
 # combine datasets
-lie_leaderlist_v2 <- read_excel('/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/LIE_LAE_Merging/lie_leader_list.xlsx')
-lae_raw <- read.csv('/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/LIE_LAE_Merging/LAE_DATA.csv')
+lie_leaderlist_v2 <- read_excel('/LIE_LAE_Merging/lie_leader_list.xlsx')
+lae_raw <- read.csv('/LIE_LAE_Merging/LAE_DATA.csv')
 
 # Merge LIE and LAE
 lae_raw_subset <- lae_raw[, c("Leader", "PoliticalUpheaval.", "Destination1", "Destination2", "Destination3")]
@@ -48,5 +48,5 @@ merged_LAE <- merge(
 merged_LAE$LAE_Name <- NULL
 
 # download merged data as excel
-write.csv(merged_LAE,'/Users/fabiangi/Documents/Goethe Uni/Semester 2/VP Regierungschefs/Term Paper/Datasets/MergedLAE.csv', na="")
+write.csv(merged_LAE,'/MergedLAE.csv', na="")
 
